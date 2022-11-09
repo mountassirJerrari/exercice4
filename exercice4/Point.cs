30ad856083e3b2ff7c7a612ece1738a884cb52d7
MJsnater <montassirejerrari@gmail.com>
2022-11-09 19:26:35 +0100

    //

---
+++ exercice4/Point.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercice3
+{
+    internal class Point
+    {
+        private float x, y;
+        //constructeur
+        Point(float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        //setters and getters
+        public void setX(float x)
+        {
+            this.x = x;
+        }
+        public float getX()
+        {
+            return this.x;
+        }
+        public void setY(float y)
+        {
+            this.y = y;
+        }
+        public float getY()
+        {
+            return this.y;
+        }
+        //egalité
+        public bool isEqual(Point p)
+        {
+            if (p.x == this.x && p.y == this.y)
+            {
+                return true;
+            }
+            return false;
+        }
+        //translation
+        public void translate(int k)
+        {
+            setX(k * this.x);
+            setY(k * this.y);
+        }
+        //la linearité
+        public bool islinear(Point p1, Point p2, Point p3)
+        {
+            float a = (p1.y - p2.y) / (p1.x - p2.x);
+            float b = p1.y - a * p1.x;
+            if (p3.y == a * p3.x + b)
+            {
+                return true;
+            }
+
+            return false;
+
+        }
+
+    }
+}
